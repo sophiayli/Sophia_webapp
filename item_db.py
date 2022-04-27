abdc5bef2f762b419ed696fd29988f2e8fc6f674
sophiayli <19399@burnside.school.nz>
2022-04-27 14:27:26 +1200

    opened new file to import spohia_webapp database

---
+++ item_db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+DATABASE_FILE = "sophia_webapp.db"
+
+with sqlite3.connect(DATABASE_FILE)as connection:
+    cursor = connection.cursor()
+    sql = "SELECT * FROM item"
+    cursor.execute(sql)
+    results = cursor.fetchall
+    print(results)
