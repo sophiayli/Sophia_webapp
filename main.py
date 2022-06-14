a4e2f416876cefc243280cdafcf81e631d89396b
sophiayli <19399@burnside.school.nz>
2022-06-14 12:45:43 +1200

    able to join item and order_item tables together to display items in the order

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cgitb import text
+from ntpath import join
 from flask import Flask,g, redirect, render_template, request
 import sqlite3
 
@@ -34,7 +35,7 @@ def menu():
 @app.route("/orders")
 def orders():
     cursor = get_db().cursor()
-    sql = "SELECT * FROM order_item"
+    sql = "SELECT id, name, description, price FROM item INNER JOIN order_item ON item.id = order_item.item_id"
     cursor.execute(sql)
     results = cursor.fetchall()
     return render_template("order.html", results=results)
@@ -43,20 +44,16 @@ def orders():
 @app.route("/add", methods=["GET","POST"])
 def add():
     cursor = get_db().cursor()
-    new_name= request.form["item_name"]
-    new_description = request.form["item_description"]
-    new_price = int(request.form["item_price"])
-    sql = "INSERT INTO order_items(item_name, item_descriptioh, item_price) VALUES (?,?,?)"
-    cursor.execute(sql,(new_name, new_description))
+    button = str(request.form["id"])
+    insert = "INSERT INTO order_items(item_id)VALUES (?)"
+    cursor.execute(insert,(button))
     get_db().commit()
-
-    return redirect('/menu')
+    return redirect('/orders')
 
 @app.route("/delete", methods=["GET", "POST"])
 def delete():
     if request.method == "POST": 
         cursor = get_db().cursor()
-        print("****************")
         print(request.form["item_id"])
         object = str(request.form["item_id"])
         sql = "DELETE FROM order_item WHERE item_id=?"
